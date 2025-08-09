4ec0492938152cf727f54ddb1ba747266d90be37
Vivek kumar singh <vivek.reck@gmail.com>
2025-08-09 18:34:01 +0530

    fix: update commitlint configuration to use CommonJS format

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,17 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat', 'fix', 'docs', 'style', 'refactor', 
-        'test', 'chore', 'perf', 'ci', 'build', 'revert'
-      ],
-    ],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-    'subject-empty': [2, 'never'],
-    'header-max-length': [2, 'always', 100],
-  },
-}
+module.exports = { extends: ["@commitlint/config-conventional"] }
